c02fa630f18734ddf384db3843d23d35edba25a6
alex-prog4 <veselov.sania2013@yandex.ru>
2024-08-11 14:45:33 +0300

    correct lint calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,41 +1,33 @@
 #!/usr/bin/env node
-import{name, generateQuestion, interfaceBrainGames}from '../src/index.js';
-
+import {name, generateQuestion, interfaceBrainGames} from '../src/index.js';
 const compareAnswer = (answerUser, answerProgram) => {
-	const resultCompare = (answerProgram === answerUser) ? true : false;
-	const wrongAnswer = (!resultCompare) ? answerUser : 0;
-	const correctAnswer = (!resultCompare) ? answerProgram : 0;
-	return [resultCompare,
-		wrongAnswer,
-		correctAnswer
-	];
+  const resultCompare = (answerProgram === answerUser) ? true : false;
+  const wrongAnswer = (!resultCompare) ? answerUser : 0;
+  const correctAnswer = (!resultCompare) ? answerProgram : 0;
+  return [resultCompare, wrongAnswer, correctAnswer];
 };
-
 const generateNumber = () => {
-	const question = Math.floor(Math.random() * 100);
-	return question;
+  const question = Math.floor(Math.random() * 100);
+  return question;
 };
-
 const generateOperationWithNumber = (firstNumber, secondNumber) => {
-	const question = Math.floor(Math.random() * 4);
-	switch (question) {
-		case 0: return ['+', firstNumber + secondNumber];
-		case 1: return ['-', firstNumber - secondNumber];
-		case 2: return ['*', firstNumber * secondNumber];
-		default: return ['*', firstNumber * secondNumber];
-	};
+  const question = Math.floor(Math.random() * 4);
+  switch (question) {
+    case 0: return ['+', firstNumber + secondNumber];
+    case 1: return ['-', firstNumber - secondNumber];
+    case 2: return ['*', firstNumber * secondNumber];
+    default: return ['*', firstNumber * secondNumber];
+  };
 };
-
 const taskBrainGame = 'What is the result of the expression?';
 const nameUser = name(taskBrainGame);
 let round = 0;
 let nextRound = true;
-
 while (round < 3 && nextRound) {
-	const firstNumber = generateNumber();
-	const secondNumber = generateNumber();
-	const operationWithNumber = generateOperationWithNumber(firstNumber, secondNumber);
-	const task = `${firstNumber} ${operationWithNumber[0]} ${secondNumber}`;
-	nextRound = interfaceBrainGames(compareAnswer(generateQuestion(task),`${operationWithNumber[1]}`),round,nameUser);
-	round += 1;
+  const firstNumber = generateNumber();
+  const secondNumber = generateNumber();
+  const operationWithNumber = generateOperationWithNumber(firstNumber, secondNumber);
+  const task = `${firstNumber} ${operationWithNumber[0]} ${secondNumber}`;
+  nextRound = interfaceBrainGames(compareAnswer(generateQuestion(task),`${operationWithNumber[1]}`),round,nameUser);
+  round += 1;
 };
